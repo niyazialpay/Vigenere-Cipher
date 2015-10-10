cf0d83d21d8a7922f2a39baa3e9c94164d64ab78
Muhammed Niyazi Alpay <muhammed@niyazialpay.com>
2015-10-11 02:07:08 +0300

    Upload

---
+++ Source/vigeneretest/frmMain.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace vigeneretest
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtxtVigenere = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnDecode = new System.Windows.Forms.Button();
+            this.btnEncode = new System.Windows.Forms.Button();
+            this.tableLayoutPanel4 = new System.Windows.Forms.TableLayoutPanel();
+            this.txtCipherKey = new System.Windows.Forms.TextBox();
+            this.rtxtEnter = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.tableLayoutPanel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(578, 525);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.rtxtVigenere, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel4, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.rtxtEnter, 0, 0);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(572, 519);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // rtxtVigenere
+            // 
+            this.rtxtVigenere.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtVigenere.Location = new System.Drawing.Point(3, 230);
+            this.rtxtVigenere.Name = "rtxtVigenere";
+            this.rtxtVigenere.ReadOnly = true;
+            this.rtxtVigenere.Size = new System.Drawing.Size(566, 221);
+            this.rtxtVigenere.TabIndex = 3;
+            this.rtxtVigenere.Text = "";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnDecode, 1, 0);
+            this.tableLayoutPanel3.Controls.Add(this.btnEncode, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 489);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(566, 27);
+            this.tableLayoutPanel3.TabIndex = 0;
+            // 
+            // btnDecode
+            // 
+            this.btnDecode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnDecode.Location = new System.Drawing.Point(286, 3);
+            this.btnDecode.Name = "btnDecode";
+            this.btnDecode.Size = new System.Drawing.Size(277, 21);
+            this.btnDecode.TabIndex = 0;
+            this.btnDecode.Text = "Decode";
+            this.btnDecode.UseVisualStyleBackColor = true;
+            this.btnDecode.Click += new System.EventHandler(this.btnDecode_Click);
+            // 
+            // btnEncode
+            // 
+            this.btnEncode.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnEncode.Location = new System.Drawing.Point(3, 3);
+            this.btnEncode.Name = "btnEncode";
+            this.btnEncode.Size = new System.Drawing.Size(277, 21);
+            this.btnEncode.TabIndex = 1;
+            this.btnEncode.Text = "Encode";
+            this.btnEncode.UseVisualStyleBackColor = true;
+            this.btnEncode.Click += new System.EventHandler(this.btnEncode_Click);
+            // 
+            // tableLayoutPanel4
+            // 
+            this.tableLayoutPanel4.ColumnCount = 1;
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel4.Controls.Add(this.txtCipherKey, 0, 0);
+            this.tableLayoutPanel4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel4.Location = new System.Drawing.Point(3, 457);
+            this.tableLayoutPanel4.Name = "tableLayoutPanel4";
+            this.tableLayoutPanel4.RowCount = 1;
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel4.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel4.Size = new System.Drawing.Size(566, 26);
+            this.tableLayoutPanel4.TabIndex = 1;
+            // 
+            // txtCipherKey
+            // 
+            this.txtCipherKey.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtCipherKey.Location = new System.Drawing.Point(3, 3);
+            this.txtCipherKey.Name = "txtCipherKey";
+            this.txtCipherKey.Size = new System.Drawing.Size(560, 20);
+            this.txtCipherKey.TabIndex = 0;
+            this.txtCipherKey.Text = "Cipher Key";
+            this.txtCipherKey.Click += new System.EventHandler(this.txtCipherKey_Click);
+            // 
+            // rtxtEnter
+            // 
+            this.rtxtEnter.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtxtEnter.Location = new System.Drawing.Point(3, 3);
+            this.rtxtEnter.Name = "rtxtEnter";
+            this.rtxtEnter.Size = new System.Drawing.Size(566, 221);
+            this.rtxtEnter.TabIndex = 2;
+            this.rtxtEnter.Text = "";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 525);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "frmMain";
+            this.Text = "Vigenere Cipher";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel4.ResumeLayout(false);
+            this.tableLayoutPanel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.RichTextBox rtxtVigenere;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnDecode;
+        private System.Windows.Forms.Button btnEncode;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel4;
+        private System.Windows.Forms.TextBox txtCipherKey;
+        private System.Windows.Forms.RichTextBox rtxtEnter;
+    }
+}
+
---
+++ Source/vigeneretest/frmMain.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+
+namespace vigeneretest
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        static void VigenereEncrypt(ref StringBuilder s, string key)
+        {
+            for (int i = 0; i < s.Length; i++) s[i] = Char.ToUpper(s[i]);
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsLetter(s[i]))
+                {
+                    s[i] = (char)(s[i] + key[j] - 'A');
+                    if (s[i] > 'Z') s[i] = (char)(s[i] - 'Z' + 'A' - 1);
+                }
+                j = j + 1 == key.Length ? 0 : j + 1;
+            }
+        }
+
+        static void VigenereDecrypt(ref StringBuilder s, string key)
+        {
+            for (int i = 0; i < s.Length; i++) s[i] = Char.ToUpper(s[i]);
+            key = key.ToUpper();
+            int j = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (Char.IsLetter(s[i]))
+                {
+                    s[i] = s[i] >= key[j] ?
+                              (char)(s[i] - key[j] + 'A') :
+                              (char)('A' + ('Z' - key[j] + s[i] - 'A') + 1);
+                }
+                j = j + 1 == key.Length ? 0 : j + 1;
+            }
+        }
+        private string Replace(string str)
+        {
+            string[] coming = { "ı", "İ", "ö", "Ö", "ş", "Ş", "ğ", "Ğ", "ü", "Ü", "ç", "Ç" };
+            string[] newstr = { "i", "I", "o", "O", "s", "S", "g", "G", "u", "U", "c", "C" };
+            for (int i = 0; i < coming.Length; i++)
+            {
+
+                str = str.Replace(coming[i], newstr[i]);
+
+            }
+            return str;
+        }
+        private void Encode()
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(txtCipherKey.Text))
+                {
+                    MessageBox.Show("Cipher Key area is not to be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtCipherKey.Focus();
+                }
+                else
+                {
+                    StringBuilder s = new StringBuilder(Replace(rtxtEnter.Text));
+                    VigenereEncrypt(ref s, txtCipherKey.Text);
+                    rtxtVigenere.Text = s.ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message,"Error", MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void Decode()
+        {
+            try
+            {
+                if (String.IsNullOrEmpty(txtCipherKey.Text))
+                {
+                    MessageBox.Show("Cipher Key area is not to be empty!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    txtCipherKey.Focus();
+                }
+                else {
+                    StringBuilder s = new StringBuilder(rtxtEnter.Text);
+                    VigenereDecrypt(ref s, txtCipherKey.Text);
+                    rtxtVigenere.Text = s.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnEncode_Click(object sender, EventArgs e)
+        {
+            Encode();
+        }
+
+        private void btnDecode_Click(object sender, EventArgs e)
+        {
+            Decode();
+        }
+
+        private void txtCipherKey_Click(object sender, EventArgs e)
+        {
+            txtCipherKey.Clear();
+        }
+
+        private void frmMain_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Control && e.Shift && e.KeyCode == Keys.E)
+            {
+                Encode();
+            }
+            else if(e.Control && e.Shift && e.KeyCode == Keys.D)
+            {
+                Decode();
+            }
+        }
+    }
+}
